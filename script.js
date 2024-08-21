56c3f9fafe42ce9eb4860aef4379dded4850c62a
tejaswinikho <tejashwini6213@gmail.com>
2024-08-21 23:14:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Menu toggle functionality (if you have a responsive menu)
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    document.querySelector('nav ul').classList.toggle('active');
+});
+
+
